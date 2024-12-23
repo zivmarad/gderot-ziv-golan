e26290d917812ef0eacaae32cd5e0f92ff8a0a2a
zivmarad <zivmarad@gmail.com>
2024-12-23 12:03:22 +0200

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,22 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const galleryGrid = document.querySelector(".gallery-grid");
-    const imageDirectory = "images/";
+    const galleryGrid = document.getElementById("gallery-grid");
+    const imageDirectory = "images/"; // תיקיית התמונות
 
+    // רשימת התמונות בגלריה
     const images = [
         "IMG_20241031_132755.jpg",
         "IMG_20241031_122522.jpg",
         "IMG_20241121_142831.jpg",
-        "images/fance.jpg",
-        "new_image2.jpg"
+        "example1.jpg",
+        "example2.jpg"
     ];
 
-    images.forEach(image => {
+    // יצירת תמונות בגלריה
+    images.forEach((image, index) => {
         const div = document.createElement("div");
         div.className = "gallery-item";
 
         const img = document.createElement("img");
         img.src = `${imageDirectory}${image}`;
-        img.alt = `פרויקט בגלריה`;
+        img.alt = `תמונה ${index + 1}`;
         img.onclick = () => openModal(img);
 
         div.appendChild(img);
